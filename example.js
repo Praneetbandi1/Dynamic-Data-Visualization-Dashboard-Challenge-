ef24427a163b35e2926ed19bcadc501f667bd1a5
PraneetBandi123 <praneetbandi2002@gmail.com>
2024-06-25 17:33:38 -0400

    Add interactivity and tooltips to visualization

--- example.js
+++ example.js
@@ -1,18 +1,29 @@
 function stockData() {
+
+
     let csvElements
     let stockName
     let startDate
     let endDate
     let filteredArray
 
-    //Loads CSV data into csv Elements variable
+    const width = 600
+    const height = 600
+    //Creates SVG
+    d3.select('body')
+        .append('svg')
+        .attr('width', width)
+        .attr('height', height)
+
+    //Loads CSV data into csvElements variable 
     d3.csv('mock_stock_data.csv').then(
         function (d) {
             csvElements = d
 
         })
-
     document.getElementById("stockDashboard").addEventListener("submit", (event) => {
+
+
         event.preventDefault()
         stockName = document.getElementById("stockName").value
         startDate = document.getElementById("startDate").value
@@ -39,6 +50,7 @@ function stockData() {
 
 
     }
+
 
     )
 }
